701534db7963cd165a4e2a019a31885a65eb56bb
Shannja <103908889+Shannja@users.noreply.github.com>
2025-06-02 21:32:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,6 @@ app.get("/", function(request, response){
 app.listen(3000, function(){
     console.log("Started on port 3000!");
 });
+
+// adding the next thing to see if open ai will revoke it:
+const openaiplsrevoke = "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"
